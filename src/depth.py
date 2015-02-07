ffecdddbb816e1e344f0d5b7baeca79b5565d20b
Arthur Allshire <arthur.allshire@gmial.com>
2015-02-08 10:06:50 +1100

    Attempt a way to get values

--- src/depth.py
+++ src/depth.py
@@ -23,7 +23,8 @@ class TrackDepth(object):
         return array
     
     def white_threshold(self, depth_image, mask):
-        ret, thresh = cv2.threshold(depth_image, 240, 255,cv2.THRESH_BINARY_INV)
+        #this function basically removes the typical 'shadow' that is seen around objects from a kinect
+        ret, thresh = cv2.threshold(depth_image, 248, 255,cv2.THRESH_BINARY_INV)
         mask = cv2.bitwise_and(mask, thresh)
         return mask
     
@@ -31,35 +32,61 @@ class TrackDepth(object):
         (x, y, w, h) = rect
         if x is not 0:
             means = []
-            depth_image = depth_image[y: y + h, x : x + w]
+            tote_image = depth_image[y: y + h, x : x + w]
             mask = mask[y: y + h, x : x + w]
-            height, width = depth_image.shape
+            height, width = tote_image.shape
             smallest_mean_index = -1
             smallest_mean = 256
             print "Width: " + str(width) + ", Height: " + str(height)
             for column in range(0, w, self.configuration['mean_band_size']):
-                mean = cv2.mean(depth_image[:,column:column+self.configuration['mean_band_size']], mask[:,column:column+self.configuration['mean_band_size']])[0]
+                mean = cv2.mean(tote_image[:,column:column+self.configuration['mean_band_size']], mask[:,column:column+self.configuration['mean_band_size']])[0]
                 print "Mean: " + str(mean) + ", X: " + str(column)
                 means.append(mean)
                 if mean < smallest_mean:
                     smallest_mean = mean
                     smallest_mean_index = column/self.configuration['mean_band_size']
             
+            tote_far_corner_short_index = -1
             if means[0] < means[-1]:
                 print "left is short side"
-                cv2.line(depth_image, (0, 0), (0, height), (255, 255, 255),5);
+                cv2.line(tote_image, (0, 0), (0, height), (255, 255, 255),5);
+                tote_far_corner_short_index = 0
             else:
                 print "right is short side"
-                cv2.line(depth_image, (width, 0), (width, height), (255, 255, 255),5);
+                cv2.line(tote_image, (width, 0), (width, height), (255, 255, 255),5);
             
-            cv2.line(depth_image, (smallest_mean_index * self.configuration['mean_band_size'], 0), (smallest_mean_index * self.configuration['mean_band_size'], height), (255, 255, 255), 5)
+            cv2.line(tote_image, (smallest_mean_index * self.configuration['mean_band_size'], 0), (smallest_mean_index * self.configuration['mean_band_size'], height), (255, 255, 255), 5)
             #print "length: " + str(len(means))
             #print "means: " + str(means)
             #print "greatest: " + str(greatest_mean)
+            print self.calculate_offsets(depth_image, x, y, tote_image, means, smallest_mean_index, tote_far_corner_short_index)
+            return tote_image
         return depth_image
+    
+    def calculate_offsets(self, depth_image, tote_x, tote_y, tote_image, tote_means, tote_near_corner_index , tote_far_corner_short_index):
+        #calculate the numbers that we are able to send over udp to the robot code as offsets
+        tote_image_height, tote_image_width = tote_image.shape
+        depth_image_height, depth_image_width = depth_image.shape
+        z_diff = 0.0
+        if self.data_range(tote_means[1:-1]) > self.configuration['tote_range_tolerance']:
+            z_diff = (tote_means[tote_far_corner_short_index] - tote_means[tote_near_corner_index])/255.0
+        short_side_center_x = (tote_x +(tote_means[tote_far_corner_short_index] + tote_means[tote_near_corner_index]))/2
+        x_diff = 2*(short_side_center_x/depth_image_width)-1
+        y_diff = depth_image[tote_y + (tote_image_height/2), short_side_center_x]
+        return x_diff, y_diff, z_diff
+        
         
                 
-        
+    def data_range(self, array):
+        big = -256
+        small = 256
+        for element in array:
+            if element > big:
+                big = element
+            if element < small:
+                small = element
+                
+        return big - small
     
 if __name__ == "__main__":
     track_colour = TrackColour()
@@ -72,6 +99,7 @@ if __name__ == "__main__":
         depth = cv2.bitwise_and(depth, depth, mask=contour_mask)
         mask = track_depth.white_threshold(depth, contour_mask)
         sides_depth = track_depth.find_sides(depth, mask, rect)
+        depth = cv2.cvtColor(depth, cv2.COLOR_GRAY2BGR)
         cv2.imshow("Sides Depth", sides_depth)
         cv2.imshow("Depth Mask", depth)
         cv2.imshow("Colour", rect_image)
